f6fa74627991ac6b59448dbffda9cc040168e2f6
Will <will.dunwoody@gmail.com>
2020-12-08 18:25:42 +0100

    added css style to the calculator, starting javascript

---
+++ app.js
@@ -0,0 +1,26 @@
+
+function operate(a, b) {
+    add(a, b)
+}
+
+function add(a, b) {
+    let output = a + b;
+    console.log(output)
+}
+
+function subtract(a, b) {
+    let output = a - b;
+    console.log(output)
+}
+
+function multiply(a, b) {
+    let output = a * b;
+    console.log(output)
+}
+
+function divide(a, b) {
+    let output = a / b;
+    console.log(output)
+}
+
+operate(10, 5)
