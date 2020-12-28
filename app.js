46c1e385db4efac6795abecbf57c57238b86e89c
Will <will.dunwoody@gmail.com>
2020-12-28 21:15:42 +0100

    Fixed HTML. added sum function, updated display js.

--- app.js
+++ app.js
@@ -1,26 +1,90 @@
 
-function operate(a, b) {
-    add(a, b)
+const operand = document.querySelectorAll(".operand");
+const operator = document.querySelectorAll(".operator")
+const clearButton = document.querySelector(".clear")
+const equals = document.querySelector(".equals")
+
+const inputText = document.querySelector(".inputCalc")
+const outputText = document.querySelector(".outputCalc")
+
+function calculator() {
+
+    let inputOperand = '';
+    let outputCalculation = '';
+    let calculation = '';
+
+    inputText.textContent = 0
+    outputText.textContent = ''
+  
+    function appendOperand(num) {
+        if (num === 0) return
+        inputOperand = inputOperand + num;
+    }
+
+    function inputDisplay() {
+        inputText.textContent = inputOperand;
+    }
+
+    function outputDisplay(operator) {
+        if (inputOperand === '') return
+        if (outputCalculation !== '') {
+            sum();
+        }
+        calculation = operator;
+        outputCalculation = inputOperand;
+        inputOperand = '';
+        inputText.textContent = 0
+        outputText.textContent = outputCalculation + ' ' + calculation
+    }
+
+
+    function sum() {
+
+        let finalSum
+        let input = parseFloat(inputOperand)
+        let output = parseFloat(outputCalculation)
+        switch (calculation) {
+            case '+':
+                finalSum = input + output
+                break
+            case '-':
+                finalSum = input - output
+                break
+            case 'x':
+                finalSum = input * output
+                break
+            case '÷':
+                finalSum = input / output
+                break
+            default:
+                return       
+        }
+    
+        inputOperand = finalSum
+        calculation = ''
+        outputCalculation = ''
+    }
+
+    operator.forEach((button) => {
+        button.addEventListener("click", function(){
+            outputDisplay(button.textContent)
+        })
+    })
+
+    operand.forEach((button) => {
+        button.addEventListener("click", function(){
+            appendOperand(button.textContent)
+            inputDisplay()
+        })
+    })
+
+    equals.addEventListener("click", function() {
+        sum()
+        inputDisplay()
+    });
+
 }
 
-function add(a, b) {
-    let output = a + b;
-    console.log(output)
-}
+clearButton.addEventListener("click", calculator);
 
-function subtract(a, b) {
-    let output = a - b;
-    console.log(output)
-}
-
-function multiply(a, b) {
-    let output = a * b;
-    console.log(output)
-}
-
-function divide(a, b) {
-    let output = a / b;
-    console.log(output)
-}
-
-operate(10, 5)
+calculator();
